456d1a69cee959e5d6cfbf5d00e47bfe4551a8a1
ayad318 <ayad.farhat2001@icloud.com>
2024-05-01 13:19:48 +1000

    init Next.JS

---
+++ dapp/src/app/page.tsx
@@ -0,0 +1,10 @@
+import injectedModule from "@web3-onboard/injected-wallets";
+
+const injected = injectedModule();
+export default function Home() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+}
